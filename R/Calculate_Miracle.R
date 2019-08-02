f7065b76d879408874fcff984a8cbd60798544fb
Tolga Turan <turantx1@urwds-21dtbm2.abbvienet.com>
2019-08-02 16:04:51 -0700

    make public

---
+++ R/Calculate_Miracle.R
@@ -0,0 +1,41 @@
+#' Mediators of Immune Response Against Cancer in soLid microEnvironments (MIRACLE)
+#' 
+#' Calculates Geneset Enrichment for ICR geneset and Miracle scores
+#' @param matrix Numeric matrix of normalized gene expression values.
+#'      This matrix holds the "Gene/Transcript Features" on the rows and the "Samples" on the columns.
+#'      Normalization is typically carried out by methods like "EDASeq" for RNAseq data and "RMA" or "Quantile" methods for Microarrays.
+#' @param platform Character string that shows the platform for the expression matrix. Can take values of c("ens", "u133p2", "entrez", "gene"). 
+#'      The default is "ens" for Ensembl Gene Ids (ie. ENSG000..). 
+#'      If the expression matrix is based on Affymetrix U133p2 or U133A platforms, please use "u133p2"
+#'      If the expression matrix is based on Entrez Gene Ids or Gene Symbols, please use "entrez" or "gene", respectively.
+#'      If the expression matrix is based on IlluminaHT12v3 or IlluminaHT12v4 platforms, please use "ilmn"
+#'      When using platforms other than "ens", there will be some loss of information due to features not matching or multi-matching.
+#' @param center Logical of length 1. User can choose whether to mean center the expression matrix. Defaults to FALSE.
+#' @return data.frame of ICR (measure of immune infiltrate) and Miracle scores (Rows) for each sample (Columns).
+#' @author Tolga Turan, \email{tolga.turan@abbvie.com}
+#' @references \url{https://doi.org/10.1186/s40425-018-0355-5}
+#' @examples
+#' miracle_scores<-Calculate_Miracle(matrix)
+#' @import yaGST
+#' @export
+#'
+
+Calculate_Miracle<-function(matrix, platform="ens", center=FALSE){
+        if (platform=="ens"){
+                geneset_list1<-geneset_list}
+        else if (platform=="u133p2"){
+                geneset_list1<-geneset_u133p2}
+        else if (platform=="entrez"){
+                geneset_list1<-geneset_entrez}
+        else if (platform=="gene"){
+                geneset_list1<-geneset_gene}
+        else if (platform=="ilmn"){
+                geneset_list1<-geneset_ilmn}
+
+      
+	yaGST1_matrix2<-Geneset_Enrich(matrix, geneset_list1, center=center)
+        yaGST1_df1<-data.frame(t(yaGST1_matrix2))
+        yaGST1_df1$Miracle<-yaGST1_df1$Pos/yaGST1_df1$Neg
+        yaGST1_df1<-yaGST1_df1[,-c(2,3)]
+	yaGST1_df1
+        }
---
+++ R/Geneset_Enrich.R
@@ -0,0 +1,57 @@
+#' Geneset Enrichment (GSE)
+#' Calculates single sample GSE scores using "yaGST" method
+#' @param matrix Numeric matrix of normalized gene expression values with unique column and rownames. 
+#' 	This matrix holds the "Gene/Transcript Features" on the rows and the "Samples" on the columns. 
+#'	Normalization is typically carried out by methods like "EDASeq" for RNAseq data and "RMA" or "Quantile" methods for Microarrays.
+#' @param geneset_list a List of gene/Transcript features. 
+#' 	Preferably all or at least 1 feature in each geneset must be present in the rownames of the expression matrix.
+#' @param verbose Logical of length 1. User can choose to display messages. Defaults to TRUE. 
+#'	If there are large numbers of genesets it can be set to FALSE
+#' @param n_cores Integer of length 1. Specifies the number of parallel cores running the function.
+#'	Defaults to detectCores()-2
+#' @param center Logical of length 1. User can choose whether to mean center the expression matrix. Defaults to TRUE.
+#' @return data.frame holding geneset enrichment values for each geneset (columns) and for each sample (columns). 
+#'	If the geneset is of length 1, then a named vector of enrichment values.
+#'
+#' @author Tolga Turan, \email{tolga.turan@abbvie.com}
+#' @references \url{http://github.com/miccec/yaGST}
+#' @examples 
+#' normalized_enrichment_scores<-Geneset_Enrich(matrix1, geneset_list1)
+#' @import yaGST
+#' @export
+#'
+
+Geneset_Enrich<-function(matrix1, geneset_list, verbose=TRUE, center=TRUE, n_cores=detectCores()-2){
+        for (i in 1:length(geneset_list)){
+                        if (verbose) {if(any(!geneset_list[[i]] %in% rownames(matrix1))){
+                                print(paste0("IDs", geneset_list[[i]][!geneset_list[[i]] %in% rownames(matrix1)], " not present in the expression matrix for the geneset ",names(geneset_list)[i]))}
+                        else{print(paste0("All IDs are found in the matrix for the geneset: ", names(geneset_list)[i]))}}}
+        library(yaGST)
+        library(parallel)
+	if(center){
+		matrix1z<-matrix1
+		for (i in 1:nrow(matrix1z)){
+			matrix1z[i,] <-(matrix1[i,] - mean(matrix1[i,]))
+			}
+		matrix1<-matrix1z
+		rm(matrix1z)
+		}
+
+        m1t<-t(matrix1)
+        m1t_l1<-split(m1t, rownames(m1t))
+        m1t_l2<-lapply(m1t_l1, function(x) setNames(x, rownames(matrix1)))
+
+        yaGST1_list1<-mclapply(m1t_l2, function(x) {rL<-sort(x, decreasing=TRUE)
+                                                   sapply(geneset_list, function(y) mwwGST(rL, y, minLenGeneSet=1)[7:11])}, mc.cores=n_cores)
+
+
+        yaGST1_list2<-lapply(yaGST1_list1, function(x) data.frame(x))
+        yaGST1_matrix1<-sapply(yaGST1_list2, function(x) sapply(x, function(y) y[[2]]))
+        if (!is.null(dim(yaGST1_matrix1))){
+                yaGST1_matrix2<-yaGST1_matrix1[,order(order(colnames(matrix1)))]}
+        else {yaGST1_matrix2<-yaGST1_matrix1[order(order(colnames(matrix1)))]
+                 names(yaGST1_matrix2)<-gsub(paste0("\\.", names(geneset_list)), "", names(yaGST1_matrix2))
+                 }
+        yaGST1_matrix2
+        }
+
