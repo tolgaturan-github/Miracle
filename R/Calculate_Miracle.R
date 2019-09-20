7a17c9045c2ef4a98c467368b49f975df8de0b7f
Tolga Turan <turantx1@urwds-21dtbm2.abbvienet.com>
2019-09-20 10:18:28 -0700

    Edit Center Parameter

--- R/Calculate_Miracle.R
+++ R/Calculate_Miracle.R
@@ -33,9 +33,9 @@ Calculate_Miracle<-function(matrix, platform="ens", center=FALSE){
                 geneset_list1<-geneset_ilmn}
 
       
-	yaGST1_matrix2<-Geneset_Enrich(matrix, geneset_list1, center=center)
+	yaGST1_matrix2<-rbind(Geneset_Enrich(matrix, geneset_list1[1], center=TRUE),Geneset_Enrich(matrix, geneset_list1[2:3], center=center))
         yaGST1_df1<-data.frame(t(yaGST1_matrix2))
         yaGST1_df1$Miracle<-yaGST1_df1$Pos/yaGST1_df1$Neg
-        yaGST1_df1<-yaGST1_df1[,-c(2,3)]
+        #yaGST1_df1<-yaGST1_df1[,-c(2,3)]
 	yaGST1_df1
         }
